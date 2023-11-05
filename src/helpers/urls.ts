f5acb63cdaa0b977c4af18b4c35acfdd8625ed20
Prasidha Acharya <prasidha@Prasidhas-Mac-mini.local>
2023-11-06 01:38:01 +0545

    set user url

---
+++ src/helpers/urls.ts
@@ -0,0 +1,26 @@
+// ============================== USER URL =======================================
+
+export const USER_BASE_URL = "/api/user" as const;
+
+export const ADD_FRIEND_URL = "/add-friend/:userId" as const;
+
+export const REMOVE_FRIEND_URL = "/remove-friend/:userId" as const;
+
+export const UPDATE_USER_URL = "/update/:userId" as const;
+
+// ================================ END  ==========================================
+
+//=============================== CATEGORY URL ====================================
+
+export const CATEGORY_BASE_URL = "/api/category" as const;
+
+// ==============================   END   =========================================
+
+// ============================== TASK URL ========================================
+
+export const TASK_BASE_URL = "/api/task" as const;
+
+// ==============================   END    =========================================
+
+// ============================== USER URL ========================================
+// ============================== USER URL ========================================
