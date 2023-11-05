ab6bee209bb82b26ce5bd75ba12dfe4003ae200f
Prasidha Acharya <prasidha@Prasidhas-Mac-mini.local>
2023-11-06 01:39:16 +0545

    set up database and router

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,32 @@
 import express from "express";
 import mongooseDatabase from "mongoose";
 import { config } from "./config";
+import cors from "cors";
+import { USER_BASE_URL } from "./helpers/urls";
+import userRouter from "./module/user/user.router";
 
-export const app = express();
+const app = express();
 
 // Database connection
-// mongooseDatabase.connect("");
+export let dbConnection: mongooseDatabase.Connection;
 
-// const db = mongooseDatabase.connection;
-// listening to port 8081
+mongooseDatabase
+  .connect(config.MONGODB_URI ?? "")
+  .then((client) => {
+    dbConnection = client.connection;
 
-app.listen(config.PORT, () => {
-  console.log("Server is started at port", config.PORT);
-});
+    app.listen(config.PORT, () => {
+      console.log("Server is started at port", config.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err, "Something went wrong while connecting to the database.");
+  });
+
+export const db = () => dbConnection;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(USER_BASE_URL, userRouter);
