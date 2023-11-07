d95a76e51d260c2011ecb0d593380892069ced18
Prasidha Acharya <prasidha@Prasidhas-Mac-mini.local>
2023-11-08 00:21:36 +0545

    error handle and api not found error handle

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import mongooseDatabase from "mongoose";
 import { config } from "./config";
 import cors from "cors";
 import { USER_BASE_URL } from "./helpers/urls";
 import userRouter from "./module/user/user.router";
+import { STATUS_CODE } from "./helpers/statusCode";
 
 const app = express();
 
@@ -27,8 +28,23 @@ export const db = () => dbConnection;
 
 app.use(cors());
 
+// Parses incoming JSON request data
+
 app.use(express.json());
 
 app.use(USER_BASE_URL, userRouter);
 
-//TODO :HANDLE ERROR
+// handle the incorrect route
+
+app.use("*", (__: Request, _: Response, next: NextFunction) => {
+  next({ status: STATUS_CODE.NOT_FOUND, message: "API not found." });
+});
+
+//  Handling error
+
+app.use((err: any, _: Request, res: Response, __: NextFunction) => {
+  const { status, message } = err;
+  res
+    .status(status ?? STATUS_CODE.INTERNAL_SERVER)
+    .json({ success: false, message });
+});
