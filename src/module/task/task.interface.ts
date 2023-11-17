bcf0d864cfc8cc538f3e91c88a607abe2a96ffa2
Prasidha Acharya <prasidha@Prasidhas-Mac-mini.local>
2023-11-17 17:37:30 +0545

    task interface set up

---
+++ src/module/task/task.interface.ts
@@ -0,0 +1,31 @@
+import { TUserLists } from "../category/category.interface";
+
+export type ObjectValues<T> = T[keyof T];
+
+export const STATUS = {
+  COMPLETED: "COMPLETED",
+  IN_PROGRESS: "IN_PROGRESS",
+} as const;
+
+export const LABEL = {
+  PRIORITY: "PRIORITY",
+} as const;
+
+type Label = ObjectValues<typeof LABEL>;
+
+type Status = ObjectValues<typeof STATUS>;
+
+export type TRequestTasks = {
+  label: Label;
+  title: string;
+  status: Status;
+  createdAt: number;
+  createdBy: string;
+  categoryId: string;
+  description: string | null;
+  userLists: TUserLists[]; // these are the person who can see the tasks
+  estimatedHr: number | null;
+  updatedAt: number | null;
+  updatedBy: string | null;
+  taskCompletedHr: number | null;
+};
